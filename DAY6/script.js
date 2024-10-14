f1aef84c5e484430f351bc5ca93bd09454ce4d05
noman9568 <noman.22b0131078@abes.ac.in>
2024-10-14 13:04:27 +0530

    DAY6

---
+++ DAY6/script.js
@@ -0,0 +1,46 @@
+
+//arrow function
+
+
+// let a = ()=>{
+//   console.log('Hello');
+// }
+// a();
+//  let b = ()=>console.log('Hello');
+//  b();
+
+//  let c = (m,n) => {
+//   return m+n;
+//  }
+//  let a = c(2,3);
+//  let b = c('hello','world');
+//  console.log(b);
+//  console.log(a);
+
+//  let p2 = document.querySelector('.p1');
+
+
+// function c_header() {
+//   let h1 = document.querySelector('header');
+//   h1.style.color = 'Red';
+//   h1.textContent = 'FSD DAY6';
+// }
+
+
+
+
+
+
+
+// Creating and appending new element to the body
+// let newElement = () => {
+//   let newElement = document.createElement('div');
+//   newElement.textContent = 'New Element';
+//   const body = document.querySelector('body');
+//   body.appendChild(newElement);
+//   document.body.appendChild(newElement);
+// }
+
+
+
+
